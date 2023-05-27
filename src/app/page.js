36a75769efc498a7ff3d333a2ed6e43948b8d9ac
Mohamed Abedallah <mohamed.altoom@gmail.com>
2023-05-27 23:52:12 +0300

    fetch data from tmdb API

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
-import Image from 'next/image'
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+  let genre = searchParams.genere || "fetchTopRated"; 
+  
+  const api_url = `https://api.themoviedb.org/3/${
+    genre==="fetchTopRated" ? "movie/top_rated" :
+    "trending/all/week"}?api_key=${API_KEY}&language=en-us&page=1`;
+  
+  const res = await fetch(api_url,{ next: { revalidate: 10000 } });
+if(!res.ok){
+  throw new Error("Failed to fetch data");
+}
+  const data =await res.json();
+  const results =data.results;  
   return (
-      <h1 className='text-red-400'>Hello</h1>
+      <div>
+        <Results results={results}/>
+      </div>
+
   )
 }
